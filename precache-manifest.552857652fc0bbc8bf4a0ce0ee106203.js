0dd2be8bf6931c8207ca2ff4ada1ddbb62c7e37d
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-07-03 11:03:37 +0400

    Updates

---
+++ precache-manifest.552857652fc0bbc8bf4a0ce0ee106203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1286bb4216d18b2d023257f46e184748",
+    "url": "/registrationform/index.html"
+  },
+  {
+    "revision": "746673ae4a0416969a59",
+    "url": "/registrationform/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "2da1763889cbe2f7ef41",
+    "url": "/registrationform/static/css/main.0bb91a6e.chunk.css"
+  },
+  {
+    "revision": "746673ae4a0416969a59",
+    "url": "/registrationform/static/js/2.ec716508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registrationform/static/js/2.ec716508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da1763889cbe2f7ef41",
+    "url": "/registrationform/static/js/main.cd12b435.chunk.js"
+  },
+  {
+    "revision": "16bf7069b58f6519ae97",
+    "url": "/registrationform/static/js/runtime-main.01425cd9.js"
+  }
+]);
